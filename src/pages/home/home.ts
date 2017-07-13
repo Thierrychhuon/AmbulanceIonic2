189ef134073d5d1c9691105d7831d21ef6c08d86
Thierrychhuon <thierry.chhuon@yahoo.fr>
2017-07-14 03:05:47 +0700

    Beginning form + validators

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NavController, Slides } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,51 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  @ViewChild('signupSlider') signupSlider: any;
 
+  public slideOneForm: FormGroup;
+  public slideTwoForm: FormGroup;
+
+  submitAttempt: boolean = false;
+
+  constructor(public navCtrl: NavController, public formBuilder: FormBuilder) {
+
+    this.slideOneForm = this.formBuilder.group({
+      firstName: ['', Validators.compose([Validators.maxLength(30), Validators.pattern('[a-zA-Z ]*'), Validators.required])],
+      lastName: ['', Validators.compose([Validators.maxLength(30), Validators.pattern('[a-zA-Z ]*'), Validators.required])],
+      email: [''],
+      telephone: ['']
+    });
+
+    this.slideTwoForm = this.formBuilder.group({
+      username: ['', Validators.compose([Validators.required, Validators.pattern('[a-zA-Z]*'),Validators.required])],
+      privacy: ['', Validators.required],
+      bio: ['']
+    });
+
+  }
+
+  next(){
+        this.signupSlider.slideNext();
+    }
+
+  prev(){
+      this.signupSlider.slidePrev();
+  }
+
+  save(){
+    this.submitAttempt = true;
+    if(!this.slideOneForm.valid){
+        this.signupSlider.slideTo(0);
+    }
+    else if(!this.slideTwoForm.valid){
+        this.signupSlider.slideTo(1);
+    }
+    else {
+        console.log("success!")
+        console.log(this.slideOneForm.value);
+        console.log(this.slideTwoForm.value);
+    }
   }
 
 }
